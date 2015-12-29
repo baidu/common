75d1f72f2a8ead74703af99a7bd74ee25c3acdba
bluebore <shiguang31@163.com>
2015-12-29 17:18:29 +0800

    Update for dfs

--- include/string_util.h
+++ include/string_util.h
@@ -15,6 +15,40 @@
 namespace baidu {
 namespace common {
 
+static inline bool IsVisible(char c) {
+    return (c >= 0x20 && c <= 0x7E);
+}
+
+static inline char ToHex(uint8_t i) {
+    char j = 0;
+    if (i < 10) {
+        j = i + '0';
+    } else {
+        j = i - 10 + 'a';
+    }
+    return j;
+}
+
+static inline std::string DebugString(const std::string& src) {
+    size_t src_len = src.size();
+    std::string dst;
+    dst.resize(src_len << 2);
+
+    size_t j = 0;
+    for (size_t i = 0; i < src_len; i++) {
+        uint8_t c = src[i];
+        if (IsVisible(c)) {
+            dst[j++] = c;
+        } else {
+            dst[j++] = '\\';
+            dst[j++] = 'x';
+            dst[j++] = ToHex(c >> 4);
+            dst[j++] = ToHex(c & 0xF);
+        }
+    }
+    return dst.substr(0, j);
+}
+
 static inline void SplitString(const std::string& full,
                                const std::string& delim,
                                std::vector<std::string>* result) {
@@ -79,7 +113,7 @@ static inline std::string NumToString(double num) {
 
 static inline std::string HumanReadableString(int64_t num) {
     static const int max_shift = 7;
-    static const char* const prefix[max_shift] = {" ", " K", " M", " G", " T", " E", " Z"};
+    static const char* const prefix[max_shift] = {" ", " K", " M", " G", " T", " P", " E"};
     int shift = 0;
     double v = num;
     while ((num>>=10) > 0 && shift < max_shift) {
