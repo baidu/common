80f52d8de23c0c95146595f805ad0326f9c4160a
bluebore <shiguang31@163.com>
2016-01-03 22:43:52 +0800

    Add perf_test_framework

---
+++ test/perf_test_condvar.cc
@@ -0,0 +1,89 @@
+// Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#include <counter.h>
+#include <mutex.h>
+#include <thread.h>
+
+#include <stdio.h>
+#include <queue>
+#include <boost/lockfree/queue.hpp>
+
+int* ring __attribute__((aligned(64)));
+int r_len __attribute__((aligned(64))) = 102400;
+volatile long r_s __attribute__((aligned(64))) = 0;
+volatile long r_e __attribute__((aligned(64))) = 0;
+
+
+namespace baidu {
+namespace common {
+
+Mutex mu __attribute__((aligned(64)));
+int g_xx1 __attribute__((aligned(64)));
+CondVar p(&mu);
+int g_xx2 __attribute__((aligned(64)));
+CondVar c(&mu);
+Counter items __attribute__((aligned(64)));
+
+std::queue<int> queue __attribute__((aligned(64)));
+
+//boost::lockfree::queue<int, boost::lockfree::fixed_sized<false> > queue(1024000);
+Counter counter __attribute__((aligned(64)));
+
+void Consumer() {
+    //sleep(5);
+    while(1) {
+        /*while(items.Get() == 0) {
+            MutexLock lock(&mu);
+            p.Wait();
+        }*/
+        //items.Dec();
+        //int v;
+        //queue.pop();
+        //ring[atomic_add64(&r_s, 1)%r_len] = -1;
+        counter.Inc();
+        c.Signal();
+    }
+}
+
+void Producer() {
+    while (1) {
+        while (items.Get() > 100000) {
+            MutexLock lock(&mu);
+            c.Wait();
+        }
+        items.Inc();
+        //queue.push(1);
+        //ring[atomic_add64(&r_e, 1)%r_len] = 1;
+        //p.Signal();
+    }
+}
+
+int Run(int tnum) {
+    Thread* t1 = new Thread[tnum];
+    Thread* t2 = new Thread[tnum];
+    printf("Thread num: %d\n", tnum);
+    for (int i = 0; i < tnum; i++) t1[i].Start(Consumer);
+    for (int i = 0; i < tnum; i++) t2[i].Start(Producer);
+    while (1) {
+        sleep(1);
+        fprintf(stderr, "%ld %ld\n", counter.Clear(), items.Get());
+    }
+    return 0;
+}
+}
+}
+
+int main(int argc, char* argv[]) {
+    int tnum = 1;
+    if (argc > 1) {
+        tnum = atoi(argv[1]);
+    }
+    ring = new int[r_len];
+    return baidu::common::Run(tnum);
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
---
+++ test/perf_test_framework.cc
@@ -0,0 +1,109 @@
+// Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#include <counter.h>
+#include <mutex.h>
+#include <thread.h>
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <queue>
+#include <list>
+
+namespace baidu {
+namespace common {
+
+Counter counter;
+
+void Atomic() {
+    while (1) {
+        counter.Inc();
+    }
+}
+
+void TestGettime() {
+    while (1) {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        counter.Inc();
+    }
+}
+
+void TestMutex() {
+    Mutex mu;
+    while (1) {
+        {
+            mu.Lock();
+            mu.Unlock();
+        }
+        counter.Inc();
+    }
+}
+
+void TestMutexLock() {
+    Mutex mu;
+    while (1) {
+        {
+            MutexLock lock(&mu);
+        }
+        counter.Inc();
+    }
+}
+
+void TestRawMutex() {
+    pthread_mutex_t m;
+    pthread_mutex_init(&m, NULL);
+    while (1) {
+        {
+            pthread_mutex_lock(&m);
+            pthread_mutex_unlock(&m);
+        }
+        counter.Inc();
+    }
+    pthread_mutex_destroy(&m);
+}
+
+int Run(int argc, char* argv[]) {
+    if (argc < 3) {
+        printf("%s function thread_num\n", argv[0]);
+        return 0;
+    }
+    void (*task)() = Atomic;
+    if (strcmp(argv[1], "mutex") == 0) {
+        printf("Perf test mutex\n");
+        task = TestMutex;
+    } else if (strcmp(argv[1], "rawmutex") == 0) {
+        printf("Perf test raw mutex\n");
+        task = TestRawMutex;
+    } else if (strcmp(argv[1], "mutexlock") == 0) {
+        printf("Perf test MutexLock\n");
+        task = TestMutexLock;
+    } else if (strcmp(argv[1], "gettime") == 0) {
+        printf("Perf test gettimeofday\n");
+        task = TestGettime;
+    }
+
+    int thread_num = 10;
+    thread_num = atoi(argv[2]);
+
+    printf("Thread num %d\n", thread_num);
+
+    Thread* t = new Thread[thread_num];
+    for (int i = 0; i < thread_num; i++) t[i].Start(task);
+    while (1) {
+        sleep(1);
+        fprintf(stderr, "%ld\n", counter.Clear());
+    }
+    return 0;
+}
+}
+}
+
+int main(int argc, char* argv[]) {
+    return baidu::common::Run(argc, argv);
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
