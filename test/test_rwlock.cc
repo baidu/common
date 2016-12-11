abc5ccc6493d5c76d4e1199f89b6060d6cf6c571
yvxiang <yangce@baidu.com>
2016-12-11 15:24:43 +0800

    Improve unit-test for RWLock
    (#56)

--- test/test_rwlock.cc
+++ test/test_rwlock.cc
@@ -3,8 +3,11 @@
 // found in the LICENSE file.
 //
 
+#include <gtest/gtest.h>
+
 #include <stdio.h>
 #include <unistd.h>
+#include <syscall.h>
 
 #include <rw_lock.h>
 #include <thread_pool.h>
@@ -12,28 +15,72 @@
 baidu::common::RWLock rw_lock;
 
 void UnlockFunc() {
+    int32_t thread_id = syscall(__NR_gettid);
+    printf("thread %d unlock\n", thread_id);
     rw_lock.Unlock();
 }
 
-void ReadLockFunc() {
+void ReadLockFunc(bool unlock) {
     rw_lock.ReadLock();
-    sleep(10);
-    UnlockFunc();
+    int32_t thread_id = syscall(__NR_gettid);
+    printf("thread %d get read lock\n", thread_id);
+    sleep(1);
+    if (unlock) {
+        UnlockFunc();
+    }
 }
 
-void WriteLockFunc() {
+void WriteLockFunc(bool unlock) {
     rw_lock.WriteLock();
-    UnlockFunc();
+    int32_t thread_id = syscall(__NR_gettid);
+    printf("thread %d get write lock\n", thread_id);
+    sleep(1);
+    if (unlock) {
+        UnlockFunc();
+    }
 }
 
-int main() {
-    baidu::common::ThreadPool tp;
-    tp.AddTask(std::bind(ReadLockFunc));
-    tp.AddTask(std::bind(WriteLockFunc));
-    tp.AddTask(std::bind(ReadLockFunc));
-    tp.AddTask(std::bind(WriteLockFunc));
-    tp.Stop(true);
-    printf("test passed\n");
+namespace baidu {
+namespace common {
+    TEST(RWLockTest, BasicTest) {
+        baidu::common::ThreadPool tp;
+        tp.AddTask(std::bind(ReadLockFunc, true));
+        tp.AddTask(std::bind(WriteLockFunc, true));
+        tp.AddTask(std::bind(ReadLockFunc, true));
+        tp.AddTask(std::bind(WriteLockFunc, true));
+        tp.Stop(true);
+    }
+    TEST(RWLockTest, RandomTest) {
+        srand(time(NULL));
+        baidu::common::ThreadPool tp;
+        for (int i = 0; i < 20; i++) {
+            int tmp = rand() % 2;
+            if (tmp) {
+                tp.AddTask(std::bind(ReadLockFunc, true));
+            } else {
+                tp.AddTask(std::bind(WriteLockFunc, true));
+            }
+        }
+        tp.Stop(true);
+    }
+    TEST(RWLockTest, UnlockInAnotherThreadTest) {
+        baidu::common::ThreadPool tp;
+        tp.AddTask(std::bind(ReadLockFunc, false));
+        // wait for task to be executed
+        sleep(2);
+        tp.AddTask(std::bind(UnlockFunc));
+        sleep(2);
+        tp.AddTask(std::bind(WriteLockFunc, false));
+        sleep(2);
+        tp.AddTask(std::bind(UnlockFunc));
+        tp.Stop(true);
+    }
+} // namespace common
+} // namespace baidu
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
 /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
